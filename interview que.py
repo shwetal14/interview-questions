67a47bd0710461ff78c77dbbeb93b976accb7559
Shwetal Arun Chachane <95999701+shwetal14@users.noreply.github.com>
2022-02-15 16:30:22 +0530

    Add files via upload

---
+++ interview que.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+# 20 interview questions for Data Types, Operators, Conditional Statements, Looping Statements, Functions.
+
+
+
+
+get_ipython().set_next_input('1) What is the difference between lists and tuples');get_ipython().run_line_magic('pinfo', 'tuples')
+
+get_ipython().set_next_input('2) What is __init__ in Python');get_ipython().run_line_magic('pinfo', 'Python')
+
+get_ipython().set_next_input('3) What are local variables and global variables in Python');get_ipython().run_line_magic('pinfo', 'Python')
+
+get_ipython().set_next_input('4) What does [::-1] do');get_ipython().run_line_magic('pinfo', 'do')
+
+get_ipython().set_next_input('5) Do we need to declare variables with data types in Python');get_ipython().run_line_magic('pinfo', 'Python')
+
+get_ipython().set_next_input('6) What is slicing in Python');get_ipython().run_line_magic('pinfo', 'Python')
+
+get_ipython().set_next_input('7) What does len() do');get_ipython().run_line_magic('pinfo', 'do')
+
+get_ipython().set_next_input('8) Why do we need break and continue in Python');get_ipython().run_line_magic('pinfo', 'Python')
+
+get_ipython().set_next_input('9) What do you know about relational operators in Python');get_ipython().run_line_magic('pinfo', 'Python')
+
+get_ipython().set_next_input('10) What is the difference between .py and .pyc files');get_ipython().run_line_magic('pinfo', 'files')
+
+get_ipython().set_next_input('11) What does this mean: *args, **kwargs? And why would we use it');get_ipython().run_line_magic('pinfo', 'it')
+    
+get_ipython().set_next_input('12) What does elif mean');get_ipython().run_line_magic('pinfo', 'mean')
+
+get_ipython().set_next_input('13) Using a dictionary, create an equivalent to a switch case statement');get_ipython().run_line_magic('pinfo', 'statement')
+
+get_ipython().set_next_input('14) Which conditional statements are available in Python');get_ipython().run_line_magic('pinfo', 'Python')
+
+15) Which looping statements are available in Python ?
+    
+16) How do you declare a variable in Python ? 
+
+17) How to you initialize an array ?
+
+18) What is Frozen Set in Python ? 
+
+get_ipython().set_next_input('19) Name four of the main data types in Python');get_ipython().run_line_magic('pinfo', 'Python')
+
+get_ipython().set_next_input('20) Differentiate between mutable and immutable objects');get_ipython().run_line_magic('pinfo', 'objects')
+
+
+# In[ ]:
+
+
+
+
